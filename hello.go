e0556875e06d6ac72bb30debb4d58a67259621e1
bl-sapnapatil <sapna.patil@bridgelabz.com>
2021-05-03 17:22:10 +0530

    add: data types in go lang

--- hello.go
+++ hello.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    var number uint8 = 255;
+    var number1 uint16 = 266;
+    fmt.Println("Hello, World!",number,number1)
 }
