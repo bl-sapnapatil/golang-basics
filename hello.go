880d723050cacf9adec732acf8cf162d65a7a8fb
bl-sapnapatil <sapna.patil@bridgelabz.com>
2021-05-03 17:55:10 +0530

    add: Assignment Expression & Implicit vs Explicit

--- hello.go
+++ hello.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
+    /*Data types - Explicit*/
     var number uint8 = 255;
     var number1 uint16 = 266;
-    fmt.Println("Hello, World!",number,number1)
+    fmt.Println("Hello World!",number,number1)
+
+    /*Data types - Implicit*/
+    var num = 200.98
+    fmt.Printf("%T",num)
+
+    /* Assignment Expression */
+    aeOperator := 6; //short variable declarations
+    fmt.Printf("%T",aeOperator)
+
 }
