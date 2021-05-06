7d0dc2946baccd10f6aad9772b9fe40187ff9db0
bl-sapnapatil <sapna.patil@bridgelabz.com>
2021-05-06 15:33:14 +0530

    add: printing to console and fmt

--- hello.go
+++ hello.go
@@ -4,17 +4,27 @@ import "fmt"
 
 func main() {
 
-    /*Data types - Explicit*/
-    var number uint8 = 255;
-    var number1 uint16 = 266;
-    fmt.Println("Hello World!",number,number1)
+	/*Data types - Explicit*/
+	var number uint8 = 255
+	var number1 uint16 = 266
+	fmt.Println("Hello World!", number, number1)
 
-    /*Data types - Implicit*/
-    var num = 200.98
-    fmt.Printf("%T",num)
+	/*Data types - Implicit*/
+	var num = 200.98
+	fmt.Printf("Type is: %T \n", num)
 
-    /* Assignment Expression */
-    aeOperator := 6; //short variable declarations
-    fmt.Printf("%T",aeOperator)
+	/* Assignment Expression */
+	aeOperator := 6 //short variable declarations
+	fmt.Printf("Type is: %T \n", aeOperator)
+
+	/* Printing to console and fmt*/
+	fmt.Printf("check type and value: %T %v \n", 10, 10)
+	fmt.Printf("checkboolean:  %t \n", true)
+	fmt.Printf("Binary Representation: %b \n", 1025)
+	fmt.Printf("Octal Representation: %o \n", 1025)
+    fmt.Printf("Decimal Representation: %d \n", 1025)
+    fmt.Printf("String Representation: %s \n", "sapna")
+    fmt.Printf("Double String Representation: %q \n", "sapna")
+
 
 }
